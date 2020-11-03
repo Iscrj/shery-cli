ea001e0443d672343d10ba20e3d6b3c02c652036
陈瑞军 <0005856@it001882deMacBook-Pro.local>
2020-11-03 10:39:51 +0800

    feat: 修改配置

--- bin/shery-init.js
+++ bin/shery-init.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 const ora = require('ora')
 const download = require('download-git-repo')
 const { parse } = require('commander')
-const tplObj = require(`${__dirname}/../template`)
+const tplObj = require(`${__dirname}/../template.json`)
 
 program
     .usage('<tempalte-name> [project-name]')
@@ -27,13 +27,13 @@ if (!projectName) {
   return
 }
 
-url = tplObj[templateName]
+const url = tplObj[templateName]
 
 console.log(chalk.white('\n Start generating... \n'))
+console.log(url)
 // 出现加载图标
 const spinner = ora("Downloading...");
 spinner.start();
-// 执行下载方法并传入参数
 
 // 执行下载方法并传入参数
 download (
